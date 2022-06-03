e391cc6c06af5a0eb3b5d2bb1343aa52b711fd01
jongchan-hong <jongchan-hong@godo.co.kr>
2022-06-03 19:55:29 +0900

    feat: 상품명 or 상품상세설명 조회 적용,테스트 코드 작성

--- src/test/java/com/example/repository/ItemRepositoryTest.java
+++ src/test/java/com/example/repository/ItemRepositoryTest.java
@@ -60,4 +60,14 @@ class ItemRepositoryTest {
         }
     }
 
+    @Test
+    @DisplayName("상품명 or 상품상세설명 조회 테스트")
+    public void findByItemNmOrItemDetailTest(){
+        this._creatItemList();
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
 }
