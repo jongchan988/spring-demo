3415e29fa437717d8807ea1e86c9ada30d231859
jongchan-hong <jongchan-hong@godo.co.kr>
2022-06-03 15:39:59 +0900

    feat: maven 디펜던시 추가, 테스트/리얼 프로펄티즈 분리

--- src/main/java/com/example/SpringDemoApplication.java
+++ src/main/java/com/example/SpringDemoApplication.java
@@ -2,11 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@PropertySource(value = {"classpath:application.real.properties"})
 public class SpringDemoApplication {
 
     public static void main(String[] args) {
