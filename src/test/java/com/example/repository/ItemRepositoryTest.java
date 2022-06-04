880d9deea65611f7e518d140355b9e696d646b40
jongchan-hong <jongchan-hong@godo.co.kr>
2022-06-04 17:46:17 +0900

    feat: @Query nativeQuery 속성을 이용한 상품 조회 적용/테스트 코드 작성

--- src/test/java/com/example/repository/ItemRepositoryTest.java
+++ src/test/java/com/example/repository/ItemRepositoryTest.java
@@ -96,5 +96,11 @@ class ItemRepositoryTest {
         this._printItemList(itemList);
     }
 
-
+    @Test
+    @DisplayName("nativeQuery 속성을 이용한 상품 조회 테스트")
+    public void findByItemDetailByNative(){
+        this._creatItemList();
+        List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명");
+        this._printItemList(itemList);
+    }
 }
