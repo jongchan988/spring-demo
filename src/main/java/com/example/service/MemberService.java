8dd3eb682958bf93c8764d14fa46a09599016ecf
jongchan988 <jongchan988@gmail.com>
2022-06-05 20:46:17 +0900

    feat: 로그인 적용

--- src/main/java/com/example/service/MemberService.java
+++ src/main/java/com/example/service/MemberService.java
@@ -3,13 +3,17 @@ package com.example.service;
 import com.example.entity.Member;
 import com.example.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     public Member saveMember(Member member){
@@ -24,4 +28,18 @@ public class MemberService {
         }
     }
 
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email);
+        if (member == null){
+            throw new UsernameNotFoundException(email);
+        }
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
+    }
 }
