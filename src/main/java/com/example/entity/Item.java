b020886c6d1b793bf9eb2da61736d4f30b0411a7
jongchan988 <jongchan988@gmail.com>
2022-07-09 20:35:07 +0900

    feat: 주문 취소 프로세스 적용

--- src/main/java/com/example/entity/Item.java
+++ src/main/java/com/example/entity/Item.java
@@ -59,4 +59,8 @@ public class Item extends BaseEntity{
         }
         this.stockNumber = restStock;
     }
+
+    public void addStock(int stockNumber){
+        this.stockNumber += stockNumber;
+    }
 }
--- src/test/java/com/example/service/OrderServiceTest.java
+++ src/test/java/com/example/service/OrderServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import com.example.constant.ItemSellStatus;
+import com.example.constant.OrderStatus;
 import com.example.dto.OrderDto;
 import com.example.entity.Item;
 import com.example.entity.Member;
@@ -69,5 +70,23 @@ class OrderServiceTest {
         List<OrderItem> orderItemList = order.getOrderItems();
         int totalPrice = orderDto.getCount() * item.getPrice();
         assertEquals(totalPrice, order.getTotalPrice());
+    }
+
+    @Test
+    @DisplayName("주문 취소 테스트")
+    public void cancelOrder(){
+        Item item = saveItem();
+        Member member = saveMember();
+
+        OrderDto orderDto = new OrderDto();
+        orderDto.setCount(10);
+        orderDto.setItemId(item.getId());
+        Long orderId = orderService.order(orderDto, member.getEmail());
+
+        Order order = orderRepository.findById(orderId).orElseThrow(EntityNotFoundException::new);
+        orderService.cancelOrder(orderId);
+
+        assertEquals(OrderStatus.CANCEL, order.getOrderStatus());
+        assertEquals(100, item.getStockNumber());
     }
 }
